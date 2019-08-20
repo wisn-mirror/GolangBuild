58f45995df766aa13d5536ad3cee0da91f5b4c89
Wisn <wuyishun_kmk@outlook.com>
2019-08-20 19:10:46 +0800

    udpate

--- code/basic/test1.go
+++ code/basic/test1.go
@@ -3,26 +3,39 @@ package main
 import "fmt"
 
 func main() {
-	name1 ,name2 :="evey","wisn"
-	fmt.Println(name1,name2)
-	var a =2
-	var b =3
-	a,b=b,a
-	fmt.Println(a,b)
+	name := "ddd"
+	i, s := test3(&name)
+	fmt.Println(i, s)
 
+	 array2:=[]int{2,3}
+	fmt.Println(array2)
+	array1:=[]int{2,3}
+	fmt.Println(array1)
+	fmt.Println(array1==nil)
+	/**结果：
+	[]
+	true
+	*/
+}
+func test3(name *string) (int, string) {
+	return 1, *name
 }
 
+func test2() {
+	a, b := TestReturnValue()
+	fmt.Println(a, b)
+	c, _ := TestReturnValue()
+	fmt.Println(c)
+}
+func TestReturnValue() (int, int) {
+	return 10, 20
+}
 
-
-var name int
-
-var(
-	a bool
-	b string
-	c int
-	c8 int8
-	c16 int16
-	c32 int32
-	c64 int64
-	d uint
-)
+func test1() {
+	name1, name2 := "evey", "wisn"
+	fmt.Println(name1, name2)
+	var a = 2
+	var b = 3
+	a, b = b, a
+	fmt.Println(a, b)
+}
