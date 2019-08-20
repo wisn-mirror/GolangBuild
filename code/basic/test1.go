75e630998eb1c2a9c88315b76aa976380f2c9f1a
Wisn <wuyishun_kmk@outlook.com>
2019-08-20 16:33:23 +0800

    update

--- code/basic/test1.go
+++ code/basic/test1.go
@@ -1,4 +1,19 @@
-package basic
+package main
+
+import "fmt"
+
+func main() {
+	name1 ,name2 :="evey","wisn"
+	fmt.Println(name1,name2)
+	var a =2
+	var b =3
+	a,b=b,a
+	fmt.Println(a,b)
+
+}
+
+
+
 var name int
 
 var(
@@ -10,8 +25,4 @@ var(
 	c32 int32
 	c64 int64
 	d uint
-
-
-	//c []float64
-
 )
