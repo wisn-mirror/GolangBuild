233dd20ce13c4050b4c03687fd3de573ef29f963
Wisn <wuyishun_kmk@outlook.com>
2019-08-27 10:47:28 +0800

    update

--- code/basic/functest.go
+++ code/basic/functest.go
@@ -1,6 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+
+func main() {
+	taskStr:=[]string{
+		"gor testLinkFunc a ",
+		" gor testLinkFunc B",
+		"gor testLinkFunc c ",
+		" gor testLinkFunc d",
+	}
+	funcList:=[]func( string)string{
+		removeStr,
+		strings.TrimSpace,
+		strings.ToUpper,
+	}
+	excute(taskStr,funcList)
+	for _,str:=range taskStr{
+		fmt.Println(str)
+	}
+}
+func excute(list []string,listfunc []func(string)string)  {
+	for index,value :=range list{
+		for _,funcStr:=range listfunc{
+			list[index]=funcStr(value)
+		}
+	}
+}
+func removeStr(str string) string {
+	return str
+}
+
+/**
+函数变量
+ */
+func funcVar() {
+	var fu func()
+	fu = fire
+	fu()
+}
+func fire()  {
+	fmt.Println("test fire")
+}
+
 
 type Data struct {
 	complex []int
@@ -19,35 +64,28 @@ func passByValueP( data *Data)  Data{
 	return *data
 }
 
-func main() {
-	//fmt.Println(addTest(2,2))
-	in:=Data{
-		complex:[]int{3,2,2},
-		instance:InstanceData{
+//测试值传递
+func testValue() {
+	in := Data{
+		complex: []int{3, 2, 2},
+		instance: InstanceData{
 			44,
 		},
-		pstr:&InstanceData{3},
+		pstr: &InstanceData{3},
 	}
-	fmt.Printf("value  %+v\n",in)
-	fmt.Printf("value %p \n",&in)
+	fmt.Printf("value  %+v\n", in)
+	fmt.Printf("value %p \n", &in)
 	//值传递
-	result:=passByValue(in)
-	fmt.Printf("value0  %+v\n",result)
-	fmt.Printf("value0 %p \n",&result)
+	result := passByValue(in)
+	fmt.Printf("value0  %+v\n", result)
+	fmt.Printf("value0 %p \n", &result)
 	//值传递
-	result1:=passByValueP(&in)
-	result1.complex[0]=33
-	fmt.Printf("value1  %+v\n",result1)
-	fmt.Printf("value1 %p \n",&result1)
-
-	fmt.Printf("value  %+v\n",in)
-	fmt.Printf("value %p \n",&in)
-
-	fmt.Printf("value0  %+v\n",result)
-	fmt.Printf("value0 %p \n",&result)
-
-	
-}
-func addTest(a,b int) int{
-	return a+b
+	result1 := passByValueP(&in)
+	result1.complex[0] = 33
+	fmt.Printf("value1  %+v\n", result1)
+	fmt.Printf("value1 %p \n", &result1)
+	fmt.Printf("value  %+v\n", in)
+	fmt.Printf("value %p \n", &in)
+	fmt.Printf("value0  %+v\n", result)
+	fmt.Printf("value0 %p \n", &result)
 }
