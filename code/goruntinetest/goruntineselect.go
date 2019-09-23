22b2041f8ba1b8179ecab208da8094a9c557c64f
Wisn <wuyishun_kmk@outlook.com>
2019-09-23 18:07:49 +0800

    调度1

--- code/goruntinetest/goruntineselect.go
+++ code/goruntinetest/goruntineselect.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func generator() chan int {
@@ -9,7 +10,7 @@ func generator() chan int {
 	go func() {
 		i := 0
 		for {
-			//time.Sleep(time.Second)
+			time.Sleep(time.Second/2)
 			out <- i
 			i++
 		}
@@ -19,6 +20,7 @@ func generator() chan int {
 
 func workerg(id int, ch chan int) {
 	for chtemp := range ch {
+		time.Sleep(time.Second*2)
 		fmt.Printf(" id:%d, result%d \n", id, chtemp)
 	}
 }
@@ -32,13 +34,25 @@ func generWorker(id int) chan int {
 func main() {
 	var c1, c2 = generator(), generator()
 	gw1 := generWorker(1)
-	gw2 := generWorker(2)
-	for {
+	temp:=0
+	var values  []int
+	timechan:=time.After(time.Second*4)
+ 	for {
+		var worker chan int
+		if len(values) > 0 {
+			worker = gw1
+			temp = values[0]
+		}
 		select {
-		case n := <-c1:
-			gw1 <- n
-		case n := <-c2:
-			gw2 <- n
+		case temp = <-c1:
+			values = append(values, temp)
+		case temp = <-c2:
+			values = append(values, temp)
+		case worker <- temp:
+			values = values[1:]
+		case <-timechan:
+			fmt.Println(values)
+			timechan=time.After(time.Second*4)
 		}
 	}
 
